bf3a94cf74ce44e7ac7b85e97eefa3faa30d51f5
karlaogh99 <carlosgarciahernandez1999@gmail.com>
2022-01-21 11:01:10 +0000

    green: isFibonacci_Test_3

--- src/main/java/Fibonacci/Fibonacci.java
+++ src/main/java/Fibonacci/Fibonacci.java
@@ -1,12 +1,33 @@
 package Fibonacci;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Fibonacci {
-    private int number;
-    public Fibonacci( int number){
-        this.number = number;
+    private ArrayList<Integer> fibonacciArrayL;
+    public Fibonacci( ){
+        fibonacciArrayL = new ArrayList<>();
+        fibonacciArrayL.add(0);
+        fibonacciArrayL.add(1);
+        fibonacciArrayL.add(1);
+        fibonacciArrayL.add(2);
+        fibonacciArrayL.add(3);
+        fibonacciArrayL.add(5);
+        fibonacciArrayL.add(8);
+        fibonacciArrayL.add(13);
+        fibonacciArrayL.add(21);
+        fibonacciArrayL.add(34);
+        fibonacciArrayL.add(55);
+        fibonacciArrayL.add(89);
+        fibonacciArrayL.add(144);
+        fibonacciArrayL.add(233);
     }
 
+
     public boolean isFibonacci(int numberToTest){
+            if (fibonacciArrayL.contains(numberToTest)){
+                return true;
+            }
         return false;
     }
 }
--- src/test/java/FibonacciTest.java
+++ src/test/java/FibonacciTest.java
@@ -8,8 +8,8 @@ public class FibonacciTest {
 
     @Test
     public void isFibonacci_Test_3(){
-        Fibonacci fibonacci = new Fibonacci(10);
-        boolean result= fibonacci.isFibonacci(2);
+        Fibonacci fibonacci = new Fibonacci();
+        boolean result= fibonacci.isFibonacci(144);
         assertTrue(result);
     }
 }
