4affdfbdd8ed291e5990945a22fdbea9fd58e161
karlaogh99 <carlosgarciahernandez1999@gmail.com>
2022-01-21 11:02:10 +0000

    refactor: isFibonacci_Test_3

--- src/test/java/FibonacciTest.java
+++ src/test/java/FibonacciTest.java
@@ -1,14 +1,18 @@
 import Fibonacci.Fibonacci;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
 
 public class FibonacciTest {
+    private Fibonacci fibonacci;
+    @Before
+    public void setUp(){
+        fibonacci = new Fibonacci();
 
-
+    }
     @Test
     public void isFibonacci_Test_3(){
-        Fibonacci fibonacci = new Fibonacci();
         boolean result= fibonacci.isFibonacci(144);
         assertTrue(result);
     }
